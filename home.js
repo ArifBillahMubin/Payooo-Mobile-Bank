1da67c2cbcdf8f354ccd6ec1819995ea9556f3dd
arif <arifbillahmubin@gmail.com>
2025-08-24 18:13:05 +0600

    b

--- home.js
+++ home.js
@@ -1,19 +1,69 @@
 const pin = 1234;
 
+// try to reusable code 
+
+// get string value
+function getValue(str) {
+    result = document.getElementById(str).value;
+    return result;
+}
+
+// get integer value
+function getIntValue(str) {
+    const result = parseInt(document.getElementById(str).value);
+    return result;
+}
+
+// get int inner text
+function getInnerText(str) {
+    const result = parseInt(document.getElementById(str).innerText);
+    return result;
+}
+
+// set inner text
+function setInnerText(str1, value) {
+    const setText = document.getElementById(str1);
+    setText.innerText = value;
+}
+
+// toggle functionality 
+
+// form toggle
+function toggle(value) {
+    const forms = document.getElementsByClassName("form");
+    for (let form of forms) {
+        form.style.display = "none"
+    }
+    document.getElementById(value).style.display = "block"
+}
+
+// form button active color toggle 
+function toggleActive(value) {
+    const active = document.getElementsByClassName("active-btn");
+    for (let btn of active) {
+        btn.style.borderColor = "rgba(8, 8, 8, 0.1)"
+        btn.style.backgroundColor = "transparent"
+    }
+
+    document.getElementById(value).style.borderColor = "#0874F2"
+    document.getElementById(value).style.backgroundColor = "rgba(8, 116, 242, 0.05)"
+}
+
+
 // add money feature
 document.getElementById("add-money-btn")
     .addEventListener("click", function (e) {
         e.preventDefault();
-        const selectBank = document.getElementById("select-bank").value;
 
-        const accountNumber = document.getElementById("account-num").value;
+        const selectBank = getValue("select-bank");
 
-        const addAmount = parseInt(document.getElementById("add-amount").value);
+        const accountNumber = getValue("account-num");
 
-        const pinNumber = parseInt(document.getElementById("pin-number").value);
+        const addAmount = getIntValue("add-amount");
 
-        const availableBalance = parseInt(document.getElementById("available-balance").innerText);
-        console.log(selectBank)
+        const pinNumber = getIntValue("pin-number");
+
+        const availableBalance = getInnerText("available-balance");
 
         if (selectBank === "Select a Bank") {
             alert("Please select a bank")
@@ -25,6 +75,11 @@ document.getElementById("add-money-btn")
             return;
         }
 
+        // if (typeof addAmount !== "integer") {
+        //     alert("Please input integer amount");
+        //     return;
+        // }
+
         if (pinNumber !== pin) {
             alert("Please enter valid pin number")
             return;
@@ -32,7 +87,7 @@ document.getElementById("add-money-btn")
 
         const newTotalAvailableBalance = addAmount + availableBalance;
 
-        document.getElementById("available-balance").innerText = newTotalAvailableBalance;
+        setInnerText("available-balance", newTotalAvailableBalance);
     })
 
 // cash out feature
@@ -40,17 +95,22 @@ document.getElementById("withdraw-money-btn")
     .addEventListener("click", function (e) {
         e.preventDefault();
 
-        const accountNumber = document.getElementById("cash-out-account-num").value;
+        const accountNumber = getValue("cash-out-account-num");
 
-        const withdrawAmount = parseInt(document.getElementById("withdraw-amount").value);
+        const withdrawAmount = getIntValue("withdraw-amount");
 
-        const pinNumber = parseInt(document.getElementById("cash-out-pin-number").value);
+        const pinNumber = getIntValue("cash-out-pin-number");
 
-        const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+        const availableBalance = getInnerText("available-balance");
 
-        console.log(accountNumber,withdrawAmount,pinNumber,availableBalance)
+
         if (accountNumber.length !== 11) {
             alert("Please enter valid account number")
+            return;
+        }
+
+        if (typeof withdrawAmount !== "integer") {
+            alert("Please input integer amount");
             return;
         }
 
@@ -58,48 +118,45 @@ document.getElementById("withdraw-money-btn")
             alert("Please enter valid pin number")
             return;
         }
-        
+
         const newAvailableBalance = availableBalance - withdrawAmount;
 
-        document.getElementById("available-balance").innerText = newAvailableBalance
+        // document.getElementById("available-balance").innerText = newAvailableBalance
+        setInnerText("available-balance", newAvailableBalance);
 
-        
+
     })
 
 // toggling
 document.getElementById("add-btn")
-    .addEventListener("click",function(){
-        document.getElementById("cash-out-from-container").style.display = "none"
-        
-        document.getElementById("add-money-from-container").style.display = "block"
+    .addEventListener("click", function () {
+        toggle("add-money-from-container");
 
         // button color togool
-        document.getElementById("add-btn").style.borderColor = "#0874F2"
-        document.getElementById("add-btn").style.backgroundColor = "rgba(8, 116, 242, 0.05)"
+        toggleActive("add-btn");
 
-        document.getElementById("cash-out-btn").style.borderColor = "rgba(8, 8, 8, 0.1)"
-        document.getElementById("cash-out-btn").style.backgroundColor = "transparent"
-        
     })
 
 document.getElementById("cash-out-btn")
     .addEventListener("click", function () {
-        document.getElementById("cash-out-from-container").style.display = "block"
-
-        document.getElementById("add-money-from-container").style.display = "none"
+        toggle("cash-out-from-container");
 
         // button color togool
-        document.getElementById("cash-out-btn").style.borderColor = "#0874F2"
-
-        document.getElementById("cash-out-btn").style.backgroundColor = "rgba(8, 116, 242, 0.05)"
-
-        document.getElementById("add-btn").style.borderColor = "rgba(8, 8, 8, 0.1)"
-        document.getElementById("add-btn").style.backgroundColor = "transparent"
+        toggleActive("cash-out-btn");
 
     })
 
+document.getElementById("transfer-money-btn")
+    .addEventListener("click", function () {
+        toggle("Transfer-money-container");
+        
+        // button color togool
+        toggleActive("transfer-money-btn");
+    })
+
+
 // Logout 
 
-document.getElementById("log-out").addEventListener("click",function(){
-    window.location.href ="./index.html"
+document.getElementById("log-out").addEventListener("click", function () {
+    window.location.href = "./index.html"
 })
